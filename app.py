dd1165208a42e1d5af16150cec0eea7dc9c20f3a
George-Sava <73170427+George-Sava@users.noreply.github.com>
2022-02-07 06:35:29 +0200

    Fixed JWT decode

--- app.py
+++ app.py
@@ -25,6 +25,4 @@ api.add_resource(ItemList, '/items/all')
 
 
 
-if __name__ == "__main__":
-      
-    application.run(port=5000,debug=True)
+
--- run.py
+++ run.py
@@ -7,3 +7,7 @@ ma.init_app(application)
 @application.before_first_request
 def create_all():
     db.create_all(app=application)
+
+if __name__ == "__main__":
+      
+    application.run(port=5000,debug=True)
